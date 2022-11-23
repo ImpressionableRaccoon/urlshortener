3c8ec2013cda779bdbd5dd6f3b341a724f4ece2f
ImpressionableRaccoon <impressionableracoon@gmail.com>
2022-11-24 00:40:36 +0300

    refactor: RootHandler division into separate functions

--- internal/handlers/main.go
+++ internal/handlers/main.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/ImpressionableRaccoon/urlshortener/internal/storage"
 	"io"
 	"net/http"
@@ -13,42 +12,49 @@ func RootHandler(st storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			id := path.Base(r.URL.Path)
-			if id == "/" {
-				http.Error(w, "Bad request", http.StatusBadRequest)
-				return
-			}
-
-			url, err := st.Get(id)
-			if err != nil {
-				http.Error(w, "Bad request", http.StatusBadRequest)
-				return
-			}
-
-			w.Header().Set("Location", url)
-			w.WriteHeader(307)
+			RootGetHandler(w, r, st)
 		case http.MethodPost:
-			b, err := io.ReadAll(r.Body)
-			if err != nil || len(b) == 0 {
-				http.Error(w, "Bad request", http.StatusBadRequest)
-				return
-			}
-
-			index, err := st.Add(string(b))
-			if err != nil {
-				http.Error(w, "Bad request", http.StatusBadRequest)
-				return
-			}
-
-			url := "http://localhost:8080/" + index
-
-			w.WriteHeader(http.StatusCreated)
-			_, err = w.Write([]byte(url))
-			if err != nil {
-				fmt.Println(err)
-			}
+			RootPostHandler(w, r, st)
 		default:
 			http.Error(w, "Bad request", http.StatusBadRequest)
 		}
 	}
+}
+
+// RootGetHandler - обработчик GET-запросов к корню
+func RootGetHandler(w http.ResponseWriter, r *http.Request, st storage.Storage) {
+	id := path.Base(r.URL.Path)
+	if id == "/" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	url, err := st.Get(id)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", url)
+	w.WriteHeader(307)
+}
+
+// RootPostHandler - обработчик POST-запросов к корню
+func RootPostHandler(w http.ResponseWriter, r *http.Request, st storage.Storage) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil || len(b) == 0 {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	index, err := st.Add(string(b))
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	url := "http://localhost:8080/" + index
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(url))
 }
