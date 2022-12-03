34ba96a35ba2f9682b03ddf016e38a3c922da69f
ImpressionableRaccoon <impressionableracoon@gmail.com>
2022-12-03 15:01:27 +0300

    refactor: move internal/app to cmd/shortener

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/ImpressionableRaccoon/urlshortener/internal/app"
+	"github.com/ImpressionableRaccoon/urlshortener/internal/routers"
+	"github.com/ImpressionableRaccoon/urlshortener/internal/storage"
+	"log"
+	"net/http"
+)
+
+const (
+	serverAddress = ":8080" // localhost, порт 8080
 )
 
 func main() {
-	app.Start()
+	// создаем хранилище для коротких ссылок
+	st := storage.NewStorage()
+	// создадим роутер
+	r := routers.NewRouter(st)
+	// запуск сервера
+	log.Fatal(http.ListenAndServe(serverAddress, r))
 }
