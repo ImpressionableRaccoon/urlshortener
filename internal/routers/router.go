b421dc05a3c2dc6f5219b873c083fffc47258969
ImpressionableRaccoon <impressionableracoon@gmail.com>
2022-12-06 13:27:15 +0300

    refactor: remove unnecessary comments

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -7,19 +7,18 @@ import (
 )
 
 func NewRouter() (chi.Router, error) {
-	// создаем роутер
 	r := chi.NewRouter()
-	// зададим встроенные middleware, чтобы улучшить стабильность приложения
+
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	// получаем хандлер
+
 	h, err := handlers.GetHandler()
 	if err != nil {
 		return nil, err
 	}
-	// настроим маршруты
+
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", h.Post)
 		r.Get("/{ID}", h.Get)
