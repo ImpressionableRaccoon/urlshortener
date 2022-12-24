a1a181721802560c1900007f3603597dd8165250
ImpressionableRaccoon <impressionableracoon@gmail.com>
2022-12-24 21:09:41 +0300

    refactor: replace math/rand ID generator with crypto/rand

--- internal/utils/id.go
+++ internal/utils/id.go
@@ -1,10 +1,10 @@
 package utils
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
+	"math/big"
 	"strings"
-	"time"
 )
 
 const (
@@ -13,11 +13,14 @@ const (
 )
 
 func GetRandomID() (string, error) {
-	rand.Seed(time.Now().UnixNano())
-	allowedCharactersLength := int32(len(allowedCharacters))
+	allowedCharactersLength := big.NewInt(int64(len(allowedCharacters)))
 	var b strings.Builder
 	for i := 0; i < idLength; i++ {
-		_, err := fmt.Fprint(&b, string(allowedCharacters[rand.Int31n(allowedCharactersLength)]))
+		n, err := rand.Int(rand.Reader, allowedCharactersLength)
+		if err != nil {
+			return "", err
+		}
+		_, err = fmt.Fprint(&b, string(allowedCharacters[n.Int64()]))
 		if err != nil {
 			return "", err
 		}
